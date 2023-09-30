03f8a0f233f583181f09010ff5e61339f28555f2
Diego hoyos <diegohlinares2004@gmail.com>
2023-09-29 21:46:30 -0500

    Css cambios para writing

--- preguntasWriting.php
+++ preguntasWriting.php
@@ -1,149 +1,92 @@
-<style>
-    .header-pw {
-        background-color: #007bff;
-        color: #fff;
-        text-align: center;
-        padding: 10px 0;
-    }
+<?php
+// Establecer la conexión a la base de datos
+require_once 'config/Conexion.php';
 
-    .main-pw {
-        max-width: 800px;
-        margin: 0 auto;
-        padding: 20px;
-    }
+// Obtener el ID del ejercicio de la URL
+if (isset($_POST['writing']) && is_numeric($_POST['writing'])) {
 
-    /* Estilos para la vista del ejercicio */
-    .ejercicio-pw {
-        background-color: #fff;
-        border: 1px solid #ddd;
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-    }
+    $ejercicioID = $_POST['writing'];
 
-    .ejercicio-pw h2 {
-        font-size: 20px;
-    }
+    // Consulta para obtener el ejercicio y sus palabras clave, incluyendo el campo "secrip-corta"
+    $sql = "SELECT Titulo, Contenido_del_Ejercicio, `secrip-corta` FROM ejercicios WHERE ID = $ejercicioID";
+    $result = $conexion->query($sql);
 
-    .ejercicio-pw p {
-        font-size: 16px;
-        margin-bottom: 20px;
-    }
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $titulo = $row["Titulo"];
+        $contenidoEjercicio = $row["Contenido_del_Ejercicio"];
+        $secrip_corta = $row["secrip-corta"];
 
-    .ejercicio-pw label {
-        font-weight: bold;
-        width: 100%;
-    }
+        // Consulta para obtener las palabras clave del ejercicio
+        $sqlPalabrasClave = "SELECT Palabra_Clave FROM Palabras_Clave WHERE Ejercicio_ID = $ejercicioID";
+        $resultPalabrasClave = $conexion->query($sqlPalabrasClave);
 
-    .ejercicio-pw input[type="text"] {
-        width: 100%;
-        padding: 10px;
-        margin-bottom: 10px;
-        border: 1px solid #ddd;
-        border-radius: 5px;
-    }
-
-    .ejercicio-pw button {
-        background-color: #007bff;
-        color: #fff;
-        padding: 10px 20px;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: background-color 0.3s;
-    }
-
-    .ejercicio-pw button:hover {
-        background-color: #0056b3;
-    }
-</style>
-
-<div class="container">
-    <div class="x" id="x2">X</div>
-    <?php
-    // Establecer la conexión a la base de datos
-    require_once 'config/Conexion.php';
-
-    // Obtener el ID del ejercicio de la URL
-    if (isset($_POST['writing']) && is_numeric($_POST['writing'])) {
-
-        $ejercicioID = $_POST['writing'];
-
-        // Consulta para obtener el ejercicio y sus palabras clave, incluyendo el campo "secrip-corta"
-        $sql = "SELECT Titulo, Contenido_del_Ejercicio, `secrip-corta` FROM ejercicios WHERE ID = $ejercicioID";
-        $result = $conexion->query($sql);
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $titulo = $row["Titulo"];
-            $contenidoEjercicio = $row["Contenido_del_Ejercicio"];
-            $secrip_corta = $row["secrip-corta"];
-
-            // Consulta para obtener las palabras clave del ejercicio
-            $sqlPalabrasClave = "SELECT Palabra_Clave FROM Palabras_Clave WHERE Ejercicio_ID = $ejercicioID";
-            $resultPalabrasClave = $conexion->query($sqlPalabrasClave);
-
-            // Obtener todas las palabras clave en un array
-            $palabrasClaveArray = [];
-            while ($rowPalabraClave = $resultPalabrasClave->fetch_assoc()) {
-                $palabraClave = $rowPalabraClave["Palabra_Clave"];
-                $palabrasClaveArray[] = $palabraClave;
-            }
-
-            // Mezclar las palabras clave en un orden aleatorio
-            shuffle($palabrasClaveArray);
-
-            // Mostrar el título del ejercicio
-            echo '<header class="header-pw">';
-            echo '<h1>' . $titulo . '</h1>';
-            echo '</header>';
-
-            // Mostrar la descripción corta
-            echo '<section class="descripcion-corta">';
-            echo '<p>' . $secrip_corta . '</p>';
-            echo '</section>';
-
-            // Mostrar el contenido del ejercicio
-            echo '<main class="main-pw">';
-            echo '<section class="ejercicio-pw">';
-            echo '<h2>Título del Ejercicio</h2>';
-            echo '<p>' . $contenidoEjercicio . '</p>';
-
-            // Mostrar las palabras clave en orden aleatorio
-            echo '<h3>Palabras Clave:</h3>';
-            echo '<ul>';
-            foreach ($palabrasClaveArray as $posicion => $palabraClave) {
-                echo '<li>' . $palabraClave . '</li>';
-            }
-            echo '</ul>';
-
-            // Mostrar los espacios en blanco con números de posición
-            echo '<h3>Espacios en Blanco:</h3>';
-            echo '<form id="formularioRespuestas">';
-            echo '<input type="hidden" name="ejercicioID" value="' . $ejercicioID . '">';
-            if ($resultPalabrasClave->num_rows > 0) {
-                foreach ($palabrasClaveArray as $posicion => $palabraClave) {
-                    echo '<label for="respuesta_' . ($posicion + 1) . '">Espacio en blanco ' . ($posicion + 1) . ':</label>';
-                    echo '<input type="text" id="respuesta_' . ($posicion + 1) . '" name="respuestas[' . ($posicion + 1) . ']" required>';
-                }
-            } else {
-                echo '<p>No se encontraron palabras clave para este ejercicio.</p>';
-            }
-            echo '<button id="btnMostrarModal" type="submit">Enviar Respuestas</button>';
-            echo '</form>';
-
-            echo '</section>';
-            echo '</main>';
-        } else {
-            echo '<p>Ejercicio no encontrado.</p>';
+        // Obtener todas las palabras clave en un array
+        $palabrasClaveArray = [];
+        while ($rowPalabraClave = $resultPalabrasClave->fetch_assoc()) {
+            $palabraClave = $rowPalabraClave["Palabra_Clave"];
+            $palabrasClaveArray[] = $palabraClave;
         }
-    } else {
-        echo '<p>ID de ejercicio no válido.</p>';
-    }
 
-    // Cerrar la conexión a la base de datos
-    $conexion->close();
-    ?>
+        // Mezclar las palabras clave en un orden aleatorio
+        shuffle($palabrasClaveArray);
+?>
+<section class="fores">
+        <section class="ares">
+            <div class="palabras">
+                <section class="palar">
+                    <h2>Palabras Clave</h2>
+                    <section class="scrall">
+                        <?php
+                        foreach ($palabrasClaveArray as $posicion => $palabraClave) {
+                            echo '<li>' . $palabraClave . '</li>';
+                        }
+                        ?>
+                    </section>
+                </section>
+            </div>
+            <div class="text-date">
+                <section class="texto-comple">
+                    <div class="x" id="x2">X</div>
+                    <h2><?php echo $titulo ?></h2>
+                    <p><?php echo $secrip_corta ?></p>
+                    <section class="fades">
+                        <div class="over-croll">
+                            <?php echo $contenidoEjercicio ?>
+                        </div>
+                    </section>
+                </section>
+                <section class="input-relle">
+                    <h2>Espacios para rellenar</h2>
+                    <section class="fares">
+                        <form id="formularioRespuestas" class="over-croll">
+                            <input type="hidden" name="ejercicioID" value="<?php echo $ejercicioID ?>">
+                            <?php
+                            if ($resultPalabrasClave->num_rows > 0) {
+                                foreach ($palabrasClaveArray as $posicion => $palabraClave) {
+                                    echo '<label for="respuesta_' . ($posicion + 1) . '">Espacio en blanco ' . ($posicion + 1) . ':</label>';
+                                    echo '<input type="text" id="respuesta_' . ($posicion + 1) . '" name="respuestas[' . ($posicion + 1) . ']" required>';
+                                }
+                            } else {
+                                echo '<p>No se encontraron palabras clave para este ejercicio.</p>';
+                            }
+                            echo '<button id="btnMostrarModal" type="submit">Enviar Respuestas</button>';
+                            ?>
+                        </form>
+                    </section>
+                </section>
+            </div>
+        </section>
+        </section>
+<?php
+    } else {
+        echo '<p>Ejercicio no encontrado.</p>';
+    }
+} else {
+    echo '<p>ID de ejercicio no válido.</p>';
+}
+$conexion->close();
+?>
 </div>
 
 <script>
